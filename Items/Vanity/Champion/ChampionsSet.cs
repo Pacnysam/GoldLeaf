db13ada48c7321ba6012836ced50f3575b5c48e7
Pacnysam <pacnysam@gmail.com>
2025-05-19 13:26:37 -0700

    champions set

---
+++ Items/Vanity/Champion/ChampionsSet.cs
@@ -0,0 +1,66 @@
+﻿using static Terraria.ModLoader.ModContent;
+using GoldLeaf.Core;
+using static GoldLeaf.Core.Helper;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using GoldLeaf.Items.Grove;
+using System.Diagnostics.Metrics;
+using System;
+using GoldLeaf.Effects.Dusts;
+using Terraria.Graphics.Effects;
+using Terraria.DataStructures;
+using GoldLeaf.Tiles.Grove;
+using ReLogic.Content;
+
+namespace GoldLeaf.Items.Vanity.Champion
+{
+    [AutoloadEquip(EquipType.Front, EquipType.Back)]
+    public class ChampionsCloak : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 34;
+
+            Item.value = Item.buyPrice(0, 5, 0, 0);
+            Item.rare = ItemRarityID.Orange;
+
+            Item.accessory = true;
+            Item.vanity = true;
+        }
+    }
+
+    [AutoloadEquip(EquipType.Body)]
+    public class ChampionsBelt : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 38;
+            Item.height = 14;
+
+            Item.value = Item.buyPrice(0, 5, 0, 0);
+            Item.rare = ItemRarityID.Orange;
+
+            Item.vanity = true;
+        }
+    }
+
+    [AutoloadEquip(EquipType.Legs)]
+    public class GymShorts : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 12;
+
+            Item.value = Item.buyPrice(0, 5, 0, 0);
+            Item.rare = ItemRarityID.Orange;
+
+            Item.vanity = true;
+        }
+    }
+}
