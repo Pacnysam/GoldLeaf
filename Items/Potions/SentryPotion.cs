f527c0f9aaf9e245fb870b88bad59b3471a9af2a
Pacnysam <pacnysam@gmail.com>
2025-08-01 15:13:02 -0700

    forgot this one
    teeny sentry potion change

--- Items/Potions/SentryPotion.cs
+++ Items/Potions/SentryPotion.cs
@@ -1,22 +1,17 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using ReLogic.Content;
 using Terraria;
-using Terraria.Enums;
-using Terraria.GameContent.Drawing;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
-using Terraria.ObjectData;
 using static Terraria.ModLoader.ModContent;
 using static GoldLeaf.Core.Helper;
-using GoldLeaf.Tiles.Decor;
 using GoldLeaf.Core.CrossMod;
 
 namespace GoldLeaf.Items.Potions
 {
     public class SentryPotion : ModItem
     {
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(1);
         public override void SetStaticDefaults()
         {
             Item.ResearchUnlockCount = 20;
