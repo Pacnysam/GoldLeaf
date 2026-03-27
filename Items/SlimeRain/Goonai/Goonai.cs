433b6e4e6a4a3e86faac0f4ac54033af5d2e4e13
Pacnysam <pacnysam@gmail.com>
2026-03-27 04:27:50 -0700

    goonai visual facelift, more slime rain setup
    +minor jellyfisher visual fix

--- Items/SlimeRain/Goonai/Goonai.cs
+++ Items/SlimeRain/Goonai/Goonai.cs
@@ -24,7 +24,7 @@ using System.Collections.Generic;
 using Terraria.GameContent.Drawing;
 using static GoldLeaf.Core.CrossMod.RedemptionHelper;
 
-namespace GoldLeaf.Items.VanillaBossDrops
+namespace GoldLeaf.Items.SlimeRain.Goonai
 {
     public class Goonai : ModItem
     {
@@ -85,14 +85,17 @@ namespace GoldLeaf.Items.VanillaBossDrops
     public class GoonaiP : ModProjectile
     {
         private static Asset<Texture2D> glowTex;
+        private static Asset<Texture2D> bigGlowTex;
         public override void Load()
         {
-            glowTex = Request<Texture2D>(Texture + "Glow");
+            //glowTex = Request<Texture2D>(Texture + "Glow");
+            glowTex = Request<Texture2D>("GoldLeaf/Textures/ShineSmall");
+            bigGlowTex = Request<Texture2D>("GoldLeaf/Textures/Shine");
         }
 
         public override void SetStaticDefaults()
         {
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 10;
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 12;
             ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
 
             Projectile.AddElements([Element.Water]);
@@ -105,9 +108,6 @@ namespace GoldLeaf.Items.VanillaBossDrops
             Projectile.friendly = true;
             Projectile.tileCollide = true;
             Projectile.extraUpdates = 3;
-            
-            Projectile.ai[0] = 0;
-            Projectile.ai[1] = 0;
 
             Projectile.DamageType = DamageClass.Ranged;
         }
@@ -115,24 +115,23 @@ namespace GoldLeaf.Items.VanillaBossDrops
         public override void AI()
         {
             Projectile.rotation = Projectile.velocity.ToRotation() + 1.57f;
-            Projectile.ai[0]++;
 
-            if (Projectile.ai[0] >= 60)
+            if (Projectile.Counter() >= 60)
             {
                 //Projectile.extraUpdates = 3;
-                Projectile.ai[1] += 0.01f;
+                Projectile.ai[0] += 0.01f;
 
                 Projectile.velocity.X *= 0.97f;
-                Projectile.velocity.Y += Projectile.ai[1];
+                Projectile.velocity.Y += Projectile.ai[0];
             }
             else 
             {
                 //Projectile.extraUpdates = 2;
                 Projectile.velocity *= 0.97f;
             }
-            if (Projectile.ai[0] >= 60 && Projectile.velocity.Y > 5 && Main.rand.NextBool(4))
+            if (Projectile.Counter() >= 60 && Projectile.velocity.Y > 5 && Main.rand.NextBool(4))
             {
-                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustType<LightDust>(), 0, 0, 0, new Color(255, 255, 118), Main.rand.NextFloat(0.4f, 0.7f));
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustType<LightDust>(), 0, 0, 0, new Color(255, 225, 131).Alpha(120), Main.rand.NextFloat(0.6f, 0.75f));
                 dust.velocity = Projectile.velocity * Main.rand.NextFloat(0.1f, 0.4f);
                 //if (dust.velocity.Y > 3f) dust.velocity.Y = 3f;
                 dust.velocity.X = 0;
@@ -143,23 +142,15 @@ namespace GoldLeaf.Items.VanillaBossDrops
 
         public override void ModifyDamageHitbox(ref Rectangle hitbox)
         {
-            if (Projectile.ai[0] >= 60 && Projectile.velocity.Y > 5)
+            if (Projectile.Counter() >= 60 && Projectile.velocity.Y > 5)
             {
                 hitbox.Inflate(14, 8);
             }
         }
 
-        /*public override void ModifyHitPlayer(Player target, ref Player.HurtModifiers modifiers)
-        {
-            if (Projectile.Center.Y <= target.Center.Y && Projectile.velocity.Y > 5 && Projectile.ai[0] >= 60)
-            {
-                modifiers.FinalDamage *= 3;
-            }
-        }*/
-
         public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
         {
-            if (Projectile.Center.Y <= target.Center.Y && Projectile.velocity.Y > 5 && Projectile.ai[0] >= 60) 
+            if (Projectile.Center.Y <= target.Center.Y && Projectile.velocity.Y > 5 && Projectile.Counter() >= 60) 
             {
                 modifiers.SetCrit();
             }
@@ -176,9 +167,14 @@ namespace GoldLeaf.Items.VanillaBossDrops
                     player.GetItem(player.whoAmI, new Item(ItemType<Goonai>()), new GetItemSettings(false, true));
                 }
 
-                ParticleOrchestrator.RequestParticleSpawn(clientOnly: true, ParticleOrchestraType.SilverBulletSparkle,
+                /*ParticleOrchestrator.RequestParticleSpawn(clientOnly: true, ParticleOrchestraType.SilverBulletSparkle,
                                         new ParticleOrchestraSettings { PositionInWorld = Projectile.Center },
-                                        Projectile.owner);
+                                        Projectile.owner);*/
+
+                Dust dust = Dust.NewDustPerfect(Projectile.Center, DustType<TwinkleDust>(), Vector2.Zero, Main.rand.Next(18, 25), new Color(255, 225, 131).Alpha(120), Main.rand.NextFloat(2.5f, 3.5f));
+                dust.rotation = Main.rand.NextFloat(MathHelper.TwoPi);
+                dust.fadeIn = 0.5f;
+                dust.customData = new LightDust.LightDustData(0.9f, Main.rand.NextFloat(0.1f, 0.25f).RandNeg());
 
                 if (Main.myPlayer == Projectile.owner)
                     SoundEngine.PlaySound(new SoundStyle("GoldLeaf/Sounds/SE/Overwatch/Headshot") { Volume = 0.55f }, Main.player[Projectile.owner].Center);
@@ -193,24 +189,29 @@ namespace GoldLeaf.Items.VanillaBossDrops
 
         public override bool PreDraw(ref Color lightColor)
         {
-            if (Projectile.ai[0] >= 60 && Projectile.velocity.Y > 5)
+            if (Projectile.Counter() >= 60 && Projectile.velocity.Y > 5)
             {
-                Vector2 drawOrigin = new(glowTex.Width() * 0.5f, Projectile.height * 0.5f);
-                Color color = new(240, 194, 50) { A = 0 };
+                Projectile.localAI[0] += 0.05f;
+
+                Texture2D weaponTexture = TextureAssets.Projectile[Projectile.type].Value;
+                Color color = new Color(255, 225, 131).Alpha(120);
+
+                //afterimage
                 for (int k = 0; k < Projectile.oldPos.Length; k++)
                 {
                     var effects = Projectile.direction == -1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
-                    Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + drawOrigin/* + new Vector2(0f, Projectile.gfxOffY)*/;
+                    Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + new Vector2(weaponTexture.Width/2f, -weaponTexture.Height/2f)/* + new Vector2(0f, Projectile.gfxOffY)*/;
                     
-                    Main.spriteBatch.Draw(glowTex.Value, drawPos, null, color * (0.6f - (0.06f * k )), Projectile.rotation, drawOrigin, Projectile.scale * (1.15f - (0.075f * k)), effects, 0f);
+                    Main.EntitySpriteDraw(glowTex.Value, drawPos, null, color * Math.Min(Projectile.localAI[0] * 3f, 1f) * (0.6f - 0.06f * k ), Projectile.rotation, glowTex.Size()/2f, Projectile.scale * (1.25f - 0.075f * k), effects);
                 }
+                Main.EntitySpriteDraw(bigGlowTex.Value, Projectile.Center + new Vector2(0, -6) - Main.screenPosition, null, color * Math.Min(Projectile.localAI[0] * 3f, 1f), Projectile.rotation, bigGlowTex.Size() / 2f, Projectile.scale * 1.25f, SpriteEffects.None);
             }
             return true;
         }
 
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
-            if (Projectile.ai[0] < 60)
+            if (Projectile.Counter() < 60)
             {
                 if (Projectile.velocity.X != oldVelocity.X)
                 {
@@ -235,7 +236,7 @@ namespace GoldLeaf.Items.VanillaBossDrops
         {
             for (int k = 0; k < 18; ++k)
             {
-                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustType<SlimeDustBlue>(), Main.rand.NextFloat(-1.5f, 1.5f) + (-Projectile.velocity.X / 5), Main.rand.NextFloat(-0.9f, 0.9f) + (-Projectile.velocity.Y / 5), 0, Color.White, Main.rand.NextFloat(0.8f, 1.2f));
+                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustType<SlimeDustBlue>(), Main.rand.NextFloat(-1.5f, 1.5f) + -Projectile.velocity.X / 5, Main.rand.NextFloat(-0.9f, 0.9f) + -Projectile.velocity.Y / 5, 0, Color.White, Main.rand.NextFloat(0.8f, 1.2f));
                 Main.dust[dust].alpha = 175;
                 if (Main.rand.NextBool(2)) Main.dust[dust].alpha += 25;
                 if (Main.rand.NextBool(2)) Main.dust[dust].alpha += 25;
